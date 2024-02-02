6be2bb718f75684a5996c2550ddbbdcb0e009bdb
himanshusrivastava009 <himanshu009@outlook.in>
2024-02-02 14:34:56 +0530

    adding serial version of Custom Exception Class

--- src/main/java/com/selenium/webelements/utilities/StudentCustomException.java
+++ src/main/java/com/selenium/webelements/utilities/StudentCustomException.java
@@ -3,6 +3,8 @@ package com.selenium.webelements.utilities;
 public class StudentCustomException extends Exception{
 
 	
+	private static final long serialVersionUID = 1L;
+
 	public StudentCustomException(String message){
 		
 		super(message);
